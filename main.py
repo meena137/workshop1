43fff5169f43019a3ddb761499d047862b9ad1f7
meena137 <meenagolla137@gmail.com>
2025-03-21 12:51:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ st.write("this is a simple Streamlit app.")
 st.button("say hello")
 st.text("hello, streamlit")
 st.text_input("please enter your name")
+
+st.radio("gender",["male","female"],index=None)
+
+if name:
+  st.write(f'hello,{name}!')
+if st.file_uploader('pls upload file:',type=['txt,'csv']):
+  st.write("thanks for uploading")
